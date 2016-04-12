3fc0d3cd38a6ceddeb397641ee6f33516e4dfee0
dinhvancntt <dinhvancntt@gmail.com>
2016-04-12 23:43:38 +0700

    Initial commit

---
+++ PhuongAnhJSC/PhuongAnhJSC/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PhuongAnhJSC.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ PhuongAnhJSC/PhuongAnhJSC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhuongAnhJSC.MvcApplication" Language="C#" %>
